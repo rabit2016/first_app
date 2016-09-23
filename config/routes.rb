f757e15e5924dece8529832e6dcaed9e2cea2cde
rabit <angel_2022@outlook.com>
2016-09-23 16:49:50 +0800

    Finsh sign-in-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
 
   resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+
 #  get 'users/new'
 
 root to: 'static_pages#home'
-match '/signup',to: 'users#new',via: 'get'
-match '/help',to: 'static_pages#help',via: 'get'
-match '/about',to: 'static_pages#about',via: 'get'
+
+match '/signup',to: 'users#new',              via: 'get'
+match '/signin',to: 'sessions#new',           via: 'get'
+match '/signout',to:'sessions#destroy',       via: 'delete'
+
+match '/about',to: 'static_pages#about',      via: 'get'
+match '/help',to: 'static_pages#help',        via: 'get'
 match '/contact', to: 'static_pages#contact', via: 'get'
 
 #  get 'static_page/about'
